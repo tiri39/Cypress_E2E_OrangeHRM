dccf47161dc12272e1f85c8378e62c4323d7c1ae
tiri39 <etisadiaafrin@gmail.com>
2024-09-02 01:05:46 +0600

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  chromeWebSecurity:false,
+  // reporter: 'cypress-mochawesome-reporter',
+  e2e: {
+    baseUrl:"https://opensource-demo.orangehrmlive.com//",
+    watchForFileChanges:false,
+    autoRefresh:false,
+    // testIsolation:false,
+    // defaultCommandTimeout: 5000,
+    setupNodeEvents(on, config) {
+      // require('cypress-mochawesome-reporter/plugin')(on);
+      config.specPattern = [
+        'cypress/e2e/ohrm_e2e.cy.js', 
+      ]
+      return config;
+    },
+  },
+});
